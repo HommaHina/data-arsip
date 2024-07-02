37508aa38172dc6751b14730aa4ae88b2988ddcd
usman <usman2202.fti@gmail.com>
2024-07-02 13:57:57 +0800

    first commit

---
+++ database/migrations/2024_07_02_042650_create_data_pegawais_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->text('pasfoto');
+            $table->text('nip');
+            $table->text('nama');
+            $table->text('nohp');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_pegawais');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () { return view('login'); })->name('login');
+Route::post('/login_aksi', [utamaController::class,'login_aksi'])->name('login_aksi');
+Route::get('/logout', [utamaController::class,'logout'])->name('logout');
